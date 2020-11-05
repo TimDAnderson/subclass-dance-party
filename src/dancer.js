3b19b26e2329a82bade90247fd65f1e088f68381
Tim Anderson <tdanderson805@gmail.com>
2020-11-04 20:45:56 -0800

    working on refactor

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,46 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
+  console.log('in the makeDancer function');
+  //var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
 
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+  console.log('logging this');
+  console.log(this);
+  console.log('logging this.step');
+  console.log(this.step);
 
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
+  //I can run the following in the console var dancer = new makeDancer()
+
+
+  this.step.call(); // <-- need to add a .call here?  what is the thisArg?
+
+
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(top, left);
 
-  return dancer;
+  //return dancer;
 };
+
+
+
+makeDancer.prototype.step = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step, timeBetweenSteps); //not sure about set timeout
+};
+
+makeDancer.prototype.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
+    console.log('a button has been pressed');
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
      * As long as the "data-dancer-maker-function-name" attribute of a
