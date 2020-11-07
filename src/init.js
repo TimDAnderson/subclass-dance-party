bd409235394924768cb81746687dc3c75d6291e4
Tim Anderson <tdanderson805@gmail.com>
2020-11-07 15:23:07 -0800

    Complete dancer interactions

--- src/init.js
+++ src/init.js
@@ -24,13 +24,13 @@ $(document).ready(function() {
     // make a dancer with a random position
     console.log($('body').height());
     //height is 1041, figure out random between 400 and 1041 instead
-    var screenHeight = (641 * Math.random()) + 400;
+    var screenHeight = (641 * Math.random()) + 400 - 70;
     console.log('this is the height in window');
     console.log(screenHeight);
 
 
 
-    var screenWidth = $('body').width() * Math.random();
+    var screenWidth = ($('body').width() * Math.random()) - 200;
     console.log('this is the width in window');
     console.log(screenWidth);
 
@@ -43,7 +43,7 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
-    window.dancers.push(dancer.$node);
+    window.dancers.push([dancer.$node, screenWidth, screenHeight]);
   });
 
 
@@ -54,9 +54,9 @@ $(document).ready(function() {
 
     for (var i = 0; i < window.dancers.length; i++) {
       console.log(window.dancers[i]);
-      window.dancers[i].removeClass();
-      window.dancers[i].appendTo('.bottom');
-      window.dancers[i].addClass('movedNode');
+      window.dancers[i][0].removeClass();
+      window.dancers[i][0].appendTo('.bottom');
+      window.dancers[i][0].addClass('movedNode');
       //window.danvers[i].focus(function(){ $(this).css("all","unset"); });
     }
 
@@ -64,8 +64,36 @@ $(document).ready(function() {
   });
 
 
-  $('.dancer').on('click', function(event) {
+  $('body').on( "click", "img", function( event ) {
     console.log('clicked on a dancer!!!!!!!');
+    
+    console.log(event.clientX);
+    console.log(event.clientY);
+    console.log(window.dancers);
+    //console.log(window.dancers[0][0].left)
+
+    for (var i = 0; i < window.dancers.length; i++) {
+      var xpos = window.dancers[i][1];
+      var ypos = window.dancers[i][2];
+      var xdiff = Math.abs(event.clientX - xpos);
+      var ydiff = Math.abs(event.clientY - ypos);
+      var pythDist = Math.sqrt(Math.pow(xdiff, 2) + Math.pow(ydiff, 2));
+      window.dancers[i].push(pythDist);
+
+    }
+
+    for (var i = 0; i < window.dancers.length; i++) {
+      //if distance is less than a certain amount
+      if (window.dancers[i][3] < 300) {
+        //make css change to spinsdf
+        window.dancers[i][0].removeClass();
+        window.dancers[i][0].addClass('spinner');
+        
+
+      }
+    }
+
+
   });
 
   //click on a dancer
